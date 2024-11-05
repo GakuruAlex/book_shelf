9ae2684d1120c75331cd77773b8f94fca9d42539
alexgicimo <gakurualexgicimo@gmail.com>
2024-11-05 14:31:26 +0300

    purpose the form for new record and editing existing record

--- book_form.py
+++ book_form.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 class AddBookForm(FlaskForm):
-    book_name = StringField(label="Book Name" , validators=[DataRequired()])
+    title = StringField(label="Book Name" , validators=[DataRequired()])
     author = StringField(label="Author", validators=[DataRequired()])
     rating = StringField(label="Rating", validators=[DataRequired()])
-    add_book = SubmitField(label="Add Book", render_kw={"class":"btn btn-primary"} )
