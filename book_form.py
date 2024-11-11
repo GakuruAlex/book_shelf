af542ca6018f13b2e6e4c129881d5dd10dda7e5a
alexgicimo <gakurualexgicimo@gmail.com>
2024-11-11 15:36:24 +0300

    signup form

--- book_form.py
+++ book_form.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 class AddBookForm(FlaskForm):
     title = StringField(label="Book Name" , validators=[DataRequired()])
     author = StringField(label="Author", validators=[DataRequired()])
     rating = StringField(label="Rating", validators=[DataRequired()])
+class SignUp(FlaskForm):
+    username = StringField(label="Username", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired()])
